1fc41fe2f9dec23199a2114fd91f8c28d04ef1c8
a-andrievskikh <sn0abg0@gmail.com>
2024-08-22 17:44:53 +0500

    chore: import index.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/App'
+import '@/styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
