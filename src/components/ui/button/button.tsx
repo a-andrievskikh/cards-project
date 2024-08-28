195ac9a9a80371c4131a33c05283f9141913f0b0
a-andrievskikh <sn0abg0@gmail.com>
2024-08-28 16:26:11 +0500

    chore: rename rest to restProps

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -12,12 +12,18 @@ export type ButtonProps<T extends ElementType = 'button'> = {
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const {
+    as: Component = 'button',
+    className,
+    fullWidth,
+    variant = 'primary',
+    ...restProps
+  } = props
 
   return (
     <Component
       className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
-      {...rest}
+      {...restProps}
     />
   )
 }
