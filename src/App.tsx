a594fb1bf7a82b6aeaf406d2098e2a3b550a42a3
a-andrievskikh <sn0abg0@gmail.com>
2024-08-29 17:10:59 +0500

    refactor: change App to arrow func

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import { Button } from '@/components/ui/button'
 
-export function App() {
+export const App = () => {
   return (
-    <div>
-      <Button as={'a'} href={'/link'} variant={'primary'}>
-        Hello
-      </Button>
-      <Button href={'/link'} variant={'primary'}>
-        Hello
-      </Button>
-    </div>
+    <Button as={'a'} href={'/link'} variant={'primary'}>
+      Hello
+    </Button>
   )
 }
