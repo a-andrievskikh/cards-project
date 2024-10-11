b126461aa03129c34b6cf1c1e80939d905fe3d16
a-andrievskikh <sn0abg0@gmail.com>
2024-10-11 17:34:39 +0500

    feat: add text-field stories

--- src/components/ui/text-field/text-field.stories.tsx
+++ src/components/ui/text-field/text-field.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { TextField } from './'
 
 const meta: Meta<typeof TextField> = {
-  argTypes: {},
+  args: {},
   component: TextField,
   tags: ['autodocs'],
   title: 'Components/TextField',
@@ -14,6 +14,30 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-    children: 'input',
+    label: 'Label',
+    placeholder: 'Placeholder',
+  },
+}
+
+export const Password: Story = {
+  args: {
+    label: 'Label',
+    placeholder: 'Password',
+    type: 'password',
+  },
+}
+export const Search: Story = {
+  args: {
+    label: 'Label',
+    placeholder: 'Search',
+    type: 'search',
+  },
+}
+
+export const Error: Story = {
+  args: {
+    errorMessage: 'Error message',
+    label: 'Input with error',
+    value: 'Wrong value',
   },
 }
